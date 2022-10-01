180d31ef7598a9d01a451da62e9bed844342f0ec
Eliott Wantz <93885811+DevEliott@users.noreply.github.com>
2022-09-30 21:00:02 -0400

    basic signal and effect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use sycamore::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    sycamore::render(|cx| {
+        let state = create_signal(cx, 0);
+        let double = create_memo(cx, || *state.get() * 2);
+
+        create_effect(cx, || {
+            println!(
+                "State changed. New state value = {}, double is = {}",
+                state.get(),
+                *double.get()
+            );
+        });
+
+        view! { cx,
+            button(on:click=|_| {state.set(*state.get() + 1)}) {
+                (state.get()) " double is " (double.get())
+            }
+        }
+    });
 }
